462715778052ba4423c51ae3bf62d9099b3c7bb0
melanie <mjharv@hotmail.com>
2024-11-22 13:52:15 -0700

    tracking mah bug

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -1,14 +1,25 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
 import { accountService } from '../services/AccountService'
 import BaseController from '../utils/BaseController'
+import { trackedBugService } from '../services/TrackedBugService'
 
 export class AccountController extends BaseController {
   constructor() {
     super('account')
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
+      .get('/trackedbugs', this.getMyTrackedBugs)
       .get('', this.getUserAccount)
       .put('', this.editUserAccount)
+  }
+  async getMyTrackedBugs(request, response, next) {
+    try {
+      const userInfo = request.userInfo
+      const trackedBug = await trackedBugService.getMyTrackedBugs(userInfo.id)
+      response.send(trackedBug)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async getUserAccount(req, res, next) {
@@ -20,7 +31,7 @@ export class AccountController extends BaseController {
     }
   }
 
-   async editUserAccount(req, res, next) {
+  async editUserAccount(req, res, next) {
     try {
       const accountId = req.userInfo.id
       req.body.id = accountId
@@ -30,5 +41,5 @@ export class AccountController extends BaseController {
       next(error)
     }
   }
-  
+
 }
--- src/services/TrackedBugService.js
+++ src/services/TrackedBugService.js
@@ -1,6 +1,10 @@
 import { dbContext } from "../db/DbContext"
 
 class TrackedBugService {
+    async getMyTrackedBugs(userInfo) {
+        const trackedBugs = await dbContext.TrackedBugs.find({ accountId: userInfo }).populate('bug')
+        return trackedBugs
+    }
 
     async getTrackedBugUser(bugId) {
         const trackedBug = await dbContext.TrackedBugs.find({ bugId: bugId }).populate('tracker')
